be140e4a10a397de0cd3d450ba934bdae5e0910c
arrickx <24638167+arrickx@users.noreply.github.com>
2021-04-18 12:38:02 -0400

    v 2.1
    - update formula
    - update the result page
    - clean up the cn.js and en.js

--- pages/result/result.js
+++ pages/result/result.js
@@ -5,19 +5,19 @@ Page({
     // Get data from the previous page and put it into this page's data
     var that = this
     that.setData({
-      inputValue1:Number(options.inputValue1Data),
-      inputValue2:Number(options.inputValue2Data),
-      inputValue3:Number(options.inputValue3Data),
-      inputValue4:Number(options.inputValue4Data),
+      aScores:Number(options.inputValue1Data),
+      bScores:Number(options.inputValue2Data),
+      cScores:Number(options.inputValue3Data),
+      dScores:Number(options.inputValue4Data),
       dollarValue:parseFloat(options.dollarData),
     })
     // Calculate the final score for each player
-    const {inputValue1, inputValue2, inputValue3, inputValue4,dollarValue} = this.data
+    const {aScores, bScores, cScores, dScores,dollarValue} = this.data
     that.setData({
-      result1:(((inputValue2-inputValue1)+(inputValue3-inputValue1)+(inputValue4-inputValue1))*dollarValue).toFixed(2),
-      result2:(((inputValue1-inputValue2)+(inputValue3-inputValue2)+(inputValue4-inputValue2))*dollarValue).toFixed(2),
-      result3:(((inputValue1-inputValue3)+(inputValue2-inputValue3)+(inputValue4-inputValue3))*dollarValue).toFixed(2),
-      result4:(((inputValue1-inputValue4)+(inputValue2-inputValue4)+(inputValue3-inputValue4))*dollarValue).toFixed(2),
+      result1:(((bScores+cScores+dScores)-3*aScores)*dollarValue).toFixed(2),
+      result2:(((aScores+cScores+dScores)-3*bScores)*dollarValue).toFixed(2),
+      result3:(((aScores+bScores+dScores)-3*cScores)*dollarValue).toFixed(2),
+      result4:(((aScores+bScores+cScores)-3*dScores)*dollarValue).toFixed(2),
     })
   },
   // initial the language data from content. also update the navigation bar title
